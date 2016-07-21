eaa3bd54ddb413785199292c31b3f264df595439
NYCPROS <ben@thedesigncoop.org>
2016-07-21 21:01:56 +0000

    favicons

--- wp-content/themes/nyc-prosthodontics/header.php
+++ wp-content/themes/nyc-prosthodontics/header.php
@@ -19,23 +19,12 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
 
 		<?php // icons & favicons (http://realfavicongenerator.net/) ?>
-	    <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-57x57.png">
-	    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-60x60.png">
-	    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-72x72.png">
-	    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-76x76.png">
-	    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-114x114.png">
-	    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-120x120.png">
-	    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-144x144.png">
-	    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-152x152.png">
-	    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/library/favicons/apple-touch-icon-180x180.png">
-	    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/favicons/favicon-32x32.png" sizes="32x32">
-	    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/favicons/android-chrome-192x192.png" sizes="192x192">
-	    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/favicons/favicon-96x96.png" sizes="96x96">
-	    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/library/favicons/favicon-16x16.png" sizes="16x16">
-	    <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/library/favicons/manifest.json">
-	    <meta name="msapplication-TileColor" content="#2b5797">
-	    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/favicons/mstile-144x144.png">
-	    <meta name="theme-color" content="#ffffff">
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/manifest.json">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="theme-color" content="#ffffff">
 
 		<!--[if IE]>
 			<link rel="shortcut icon" href="favicon.ico">
