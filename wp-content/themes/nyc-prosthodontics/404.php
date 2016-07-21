b4e6a12cb29ad8652157e85ff8b13ddb73c871fc
NYCPROS <ben@thedesigncoop.org>
2016-07-21 21:13:56 +0000

    theme file cleanup, screenshot, 404, etc.

--- wp-content/themes/nyc-prosthodontics/404.php
+++ wp-content/themes/nyc-prosthodontics/404.php
@@ -4,33 +4,22 @@
 
 				<div id="inner-content" class="wrap cf">
 
-					<div id="main" class="m-all t-2of3 d-5of7 cf" role="main">
+					<div id="main" class="cf" role="main">
 
 						<article id="post-not-found" class="hentry cf">
 
 							<header class="article-header">
 
-								<h1><?php _e( 'Epic 404 - Article Not Found', 'bonestheme' ); ?></h1>
+								<h1>Page Not Found</h1>
 
 							</header>
 
 							<section class="entry-content">
 
-								<p><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'bonestheme' ); ?></p>
+								<p>We aren't able to locate the page you're looking for.</p>
+								<p>Please <a href="/contact">CONTACT US</a> if there's anything we can do to help.</p>
 
 							</section>
-
-							<section class="search">
-
-									<p><?php get_search_form(); ?></p>
-
-							</section>
-
-							<footer class="article-footer">
-
-									<p><?php _e( 'This is the 404.php template.', 'bonestheme' ); ?></p>
-
-							</footer>
 
 						</article>
 
