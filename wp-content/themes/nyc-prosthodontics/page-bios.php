962d290a8a514c06886309cab8ab3eff1bca5223
NYCPROS <ben@thedesigncoop.org>
2016-07-26 17:31:16 +0000

    bio order switch + hamburger thicken

--- wp-content/themes/nyc-prosthodontics/page-bios.php
+++ wp-content/themes/nyc-prosthodontics/page-bios.php
@@ -33,19 +33,19 @@ Template Name: Page (Bios)
 									
 									<div class="bios-block cf">
 										
-										<div class="m-all t-2of3 d-3of4 last-col cf">
-										<h2><?php the_title(); ?></h2>
-										<p class="bio-subtitle"><strong><?php the_field('subtitle'); ?></strong></p>
-										<?php the_content(); ?>
-										</div>
-									
 										<div class="m-all t-1of3 d-1of4 first-col cf">
 										<?php if ( has_post_thumbnail() ) { ?>
 										
 										<?php echo get_the_post_thumbnail($page->ID, 'large', array( 'class' => 'img-responsive' )); ?>
 										
 										<?php } // end post thumb check ?>	
-										</div>									
+										</div>
+										
+										<div class="m-all t-2of3 d-3of4 last-col cf">
+										<h2><?php the_title(); ?></h2>
+										<p class="bio-subtitle"><strong><?php the_field('subtitle'); ?></strong></p>
+										<?php the_content(); ?>
+										</div>
 
 									</div>
 
